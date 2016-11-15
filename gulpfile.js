692dc34dce41346eaf6174a33a984589d0d51070
jaredmpeterson <jared@peteyinc.com>
2016-11-15 00:12:29 -0700

    gulp styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,54 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
+var del = require('del');
 var $ = require('gulp-load-plugins')({lazy: true});
 
-gulp.task('vet', function() {
-    log('Analyzing source with JSHint and JSCS');
-
-    return gulp
-        .src(config.alljs)
-        .pipe($.if(args.verbose, $.print()))
-        .pipe($.jscs())
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
-        .pipe($.jshint.reporter('fail'));
-});
-
-gulp.task('styles', function () {
-  log('Compile Sass to CSS');
+gulp.task('vet', function () {
+  log('Analyzing source with JSHint and JSCS');
 
   return gulp
-  .src(config.sass)
-  .pipe($.sass())
-  .pipe($.autoprefixer({
-    browsers: ['last 2 version', '> 5%']
-  }))
-  .pipe(gulp.dest(config.temp));
+    .src(config.alljs)
+    .pipe($.if(args.verbose, $.print()))
+    .pipe($.jscs())
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+    .pipe($.jshint.reporter('fail'));
 });
 
-////////////
+gulp.task('styles', ['clean-styles'], function() {
+    log('Compiling Sass --> CSS');
+
+    return gulp
+        .src(config.sass)
+        .pipe($.plumber())
+        .pipe($.sass())
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function(done) {
+    clean(config.temp + '**/*.css', done);
+});
+
+gulp.task('sass-watch', function () {
+  gulp.watch([config.sass], ['styles']);
+});
+
+// Functions //
+function clean(path, done) {
+    log('Cleaning: ' + $.util.colors.blue(path));
+    del(path, done);
+}
 
 function log(msg) {
-    if (typeof(msg) === 'object') {
-        for (var item in msg) {
-            if (msg.hasOwnProperty(item)) {
-                $.util.log($.util.colors.blue(msg[item]));
-            }
-        }
-    } else {
-        $.util.log($.util.colors.blue(msg));
+  if (typeof (msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        $.util.log($.util.colors.blue(msg[item]));
+      }
     }
+  } else {
+    $.util.log($.util.colors.blue(msg));
+  }
 }
